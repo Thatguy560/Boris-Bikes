79e674889bffe7ceca41c3d3067e95ca1e3c411c
Peter Stevens <Peterstevens510@gmail.com>
2020-02-19 17:38:25 +0000

    Update to files

--- Lib/docking_station.rb
+++ Lib/docking_station.rb
@@ -1,3 +1,6 @@
+require '~/Projects/Week1/Boris-Bikes/Lib/bike.rb'
 class DockingStation
-
+  def release_bike
+   Bike.new
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,17 @@
 require 'docking_station'
-
 describe DockingStation do
 
-end
+  #personal work I've done
+  #docking_station = DockingStation.new
+  #it {expect(docking_station).to respond_to
+
+  it { is_expected.to respond_to :release_bike }
+
+  docking_station = DockingStation.new
+  bike = docking_station.release_bike
+
+  it 'responds to method "working"' do
+    # expect(bike.working?).to eq nil
+    expect(bike).to respond_to :working?
+    end
+  end
