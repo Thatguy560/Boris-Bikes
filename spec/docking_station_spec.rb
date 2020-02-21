bfca049f14d800de5c90d6907c70985dd8d8f1f5
Peter Stevens <Peterstevens510@gmail.com>
2020-02-21 11:07:00 +0000

    updated files

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,11 +7,28 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  docking_station = DockingStation.new
-  bike = docking_station.release_bike
-
-  it 'responds to method "working"' do
+  it 'responds to method "working?"' do
     # expect(bike.working?).to eq nil
-    expect(bike).to respond_to :working?
-    end
+     bike = Bike.new
+    expect(bike).to be_working
   end
+
+#  it 'responds to method "dock"' do
+#    bike = Bike.new
+#    expect{subject.dock(bike).to respond_to :dock }
+#  end
+
+  it 'docks bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.station).to eq bike #the bike that has been docked
+    # in the station
+  end
+
+  it 'raises error if no bikes' do
+  expect{subject.release_bike}.to raise_error("No Bikes avaliable")
+    end
+    it 'raises error if docking station is full' do
+    expect{subject.dock(bike)}.to raise_error("Bikes are overcapacity")
+  end
+end
