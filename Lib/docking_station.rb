4d369d4e4756352ac8808046f1b04e87de552f98
Peter Stevens <Peterstevens510@gmail.com>
2020-02-21 11:06:18 +0000

    updated files

--- Lib/docking_station.rb
+++ Lib/docking_station.rb
@@ -1,6 +1,15 @@
-require '~/Projects/Week1/Boris-Bikes/Lib/bike.rb'
+require_relative "./bike.rb"
 class DockingStation
+attr_reader :station #Allows us to read instance variables
+
   def release_bike
+    fail "No Bikes avaliable" unless @station
    Bike.new
   end
-end
+
+  def dock(bike)
+    @station = bike
+    fail "Bikes are overcapacity" unless @station
+    Bike.new
+   end
+ end
